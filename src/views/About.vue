8e21fd02f68f3f6bf2dbf0d34b2807be5e756f60
Julio Rodas <22454285+jrrmcalcio@users.noreply.github.com>
2020-11-13 16:00:36 -0600

    feat(core): add layout, pages and routes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Todotify</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo atque in,
+      doloremque neque nulla perspiciatis odio illum, aperiam eligendi aut
+      officia! Sequi eveniet velit ratione error id neque autem expedita?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo atque in,
+      doloremque neque nulla perspiciatis odio illum, aperiam eligendi aut
+      officia! Sequi eveniet velit ratione error id neque autem expedita?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo atque in,
+      doloremque neque nulla perspiciatis odio illum, aperiam eligendi aut
+      officia! Sequi eveniet velit ratione error id neque autem expedita?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo atque in,
+      doloremque neque nulla perspiciatis odio illum, aperiam eligendi aut
+      officia! Sequi eveniet velit ratione error id neque autem expedita?
+    </p>
   </div>
 </template>
