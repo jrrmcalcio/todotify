bc7267cf9368191510413e7234a7b0e7cd5205d8
Julio Rodas <22454285+jrrmcalcio@users.noreply.github.com>
2020-11-14 19:08:32 -0600

    feat(task): add delete confirmation

--- src/components/todo/Task.vue
+++ src/components/todo/Task.vue
@@ -17,18 +17,27 @@
         </v-list-item-content>
 
         <v-list-item-action>
-          <v-btn icon @click.stop="deleteTask(task)">
+          <v-btn icon @click.stop="dialogs.delete = true">
             <v-icon color="primary lighten-1">mdi-delete</v-icon>
           </v-btn>
         </v-list-item-action>
       </template>
     </v-list-item>
     <v-divider></v-divider>
+
+    <delete-confirmation
+      :task="task"
+      v-if="dialogs.delete"
+      @cancelDelete="dialogs.delete = false"
+    />
   </div>
 </template>
 
 <script>
+import DeleteConfirmation from './dialogs/DeleteConfirmation.vue'
+
 export default {
+  components: { DeleteConfirmation },
   name: 'Taks',
   props: {
     task: {
@@ -36,12 +45,14 @@ export default {
       required: true,
     },
   },
+  data: () => ({
+    dialogs: {
+      delete: false,
+    },
+  }),
   methods: {
     finishTask(id) {
       this.$store.commit('finishTask', id)
-    },
-    deleteTask(task) {
-      this.$store.dispatch('deleteTask', task)
     },
   },
 }
---
+++ src/components/todo/dialogs/DeleteConfirmation.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog :value="true" persistent max-width="290">
+    <v-card>
+      <v-card-title class="headline">
+        Delete Task?
+      </v-card-title>
+      <v-card-text>
+        Are you sure you want to delete &quot;{{ task.title }}&quot; task?
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="$emit('cancelDelete')">
+          Cancel
+        </v-btn>
+        <v-btn
+          color="red darken-1"
+          text
+          @click="$store.dispatch('deleteTask', task)"
+        >
+          Yes
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'DeleteConfirmation',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
